bdb55803b6dd0be41917d676ece105800a850bec
keyshafatyaandrea <keyshafatyaandrea@smkwikrama.sch.id>
2026-06-17 09:53:14 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-// Ini route web biasa untuk tampilan blade, bukan API.
-// Kalo guru nanya kenapa ada controller API terpisah, jawab:
-// web route di sini, API route di routes/api.php.
 
 use Illuminate\Support\Facades\Route;
 
