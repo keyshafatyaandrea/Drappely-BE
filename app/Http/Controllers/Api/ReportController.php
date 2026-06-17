cae796f472777783516e63352c98d7c3c37382bb
keyshafatyaandrea <keyshafatyaandrea@smkwikrama.sch.id>
2026-06-17 10:12:39 +0700

    Update ReportController.php

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -9,7 +9,6 @@ use Barryvdh\DomPDF\Facade\Pdf;
 
 class ReportController extends Controller
 {
-    //download struk pdf
     public function downloadReceiptPdf($id)
     {
         try {
@@ -18,7 +17,6 @@ class ReportController extends Controller
             $pdf = Pdf::loadView('exports.receipt_pdf', compact('transaction'));
             return $pdf->download("struk_{$transaction->id}.pdf");
         } catch (\Exception $e) {
-            //mengembalikan pesan error kalo transaksi ga ketemu / ada kendala
             return response()->json([
                 'status' => 'error', 
                 'message' => 'Gagal membuat file PDF: ' . $e->getMessage()
